442a75b04788162b9c3a6b4b7b6670dec91f91f7
Antoine Spahr <antoine.spahr@epfl.ch>
2019-03-21 23:42:30 +0100

    Spectrogram computation functions + plotting for all channels

--- Code/4_correlate_analysis/spectrogram_script.m
+++ Code/4_correlate_analysis/spectrogram_script.m
@@ -10,31 +10,30 @@ load('./../outputs/epoch_MI_Baseline.mat');
 
 % periodogram for all channels
 fs = epochs_MI_Baseline.sampling_frequency;
-overlap_time = 0.0625;
+non_overlap_time = 0.0625;
 window_time = 1;
 
+%% Compute ERD/ERS
 % ERD_ERS_mat : 4D array -> time x freq x trial x channel 
 ERD_ERS_mat = [];
 
 for c=1:1:size(epochs_MI_Baseline.trial, 2)
-    ERD_ERS_mat = cat(4, ERD_ERS_mat, spectrogram_from_many_epochs(squeeze(epochs_MI_Baseline.trial(:,11,:)), fs, window_time, overlap_time));
+    [ERD_ERS_chan, t, f] = spectrogram_from_many_epochs(squeeze(epochs_MI_Baseline.trial(:,c,:)), fs, window_time, non_overlap_time);
+    ERD_ERS_mat = cat(4, ERD_ERS_mat, ERD_ERS_chan);
 end
-    
-imagesc(mean(squeeze(ERD_ERS_mat(:,:,:,11)), 3))
+ 
+%% Save the matrix ERD_ERS_mat
 
+%% Plot the spectrogram averaged over all trials
+% single plot : Channel C3
+figure(1)
+spectrogram_plot(mean(squeeze(ERD_ERS_mat(:,:,:,11)), 3), t-3, f, 'C3')
 
-%%
+% all channels
+channel_names = {'FZ';'FC3';'FC1';'FCz';'FC2';'FC4';'C3';'C1';'Cz';'C2';'C4';'CP3';'CP1';'CPZ';'CP2';'CP4'};
 
-noverlap = overlap_time * fs;
-window = window_time * fs ; 
-
-tmp=squeeze(epochs_MI_Baseline.trial(1,11,:));
-[~,f,t,power] = spectrogram(tmp, window, noverlap, [], fs, 'power');
-
-% get mean base line in -3 to -1 seconds
-B1 = mean(power(:,round(size(power,2)/3)), 2);
-%B=repmat(B1,1,round(size(power,2)));
-% compute ERD/ERS [dB]
-ERD_ERS = 10*log10(power./B1);
-
-imagesc(ERD_ERS)
+figure(2)
+for c = 1:1:size(ERD_ERS_mat, 4)
+   subplot(4,4,c)
+   spectrogram_plot(mean(squeeze(ERD_ERS_mat(:,:,:,c)), 3), t-3, f, channel_names{c})
+end
