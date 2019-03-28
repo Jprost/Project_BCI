9097403a5b3777c528305303ffd1bdf8f9113ec8
Sacha Haidinger <sachahaidinger@bluewin.ch>
2019-03-28 16:01:57 +0100

    Feature extraction function

---
+++ Code/5_feature_extraction/feat_extraction.m
@@ -0,0 +1,47 @@
+function [features_mat] = feat_extraction(trials, time, win, shift, start_ERD, stop_ERD, start_ERS, stop_ERS)
+%Perform sequential Pwelch over 1s on ERD and ERS subpart of all trials, for
+%Feature Extraction
+%  
+%   Input :
+%       - trials : all trials output from epoching. 
+%                   Dim (trials x Channel x time)
+%                   WARNING : Need an epoching -3 +3 aroung MI_STOP event
+%       - time : time value associated with the epoch
+%       - win : time window in s on wich pwelch is compute
+%       - shift : time overlap in s to shift windwo and compute next Pwelch
+%       - start : Time in second from EVENT, where to start feature extr.
+%       - stop : Time in second from EVENT, where to stop feature extr.
+%                If MI-STOP event:
+%                   ERD (Class 0) : start = -2   stop = 0
+%                   ERS (Class 1) : start = 0.5 stop = 2.5
+%
+%
+%   Output :
+%       features_mat : 32 x 304 x 80   =  samples x Features x nTrials
+%       All Features (channels & freq) for all trials and all 1s window
+%       shifted (samples)
+%       feat_oneTrial : 32 x 304   concatenate both output of windowing()
+%       on ERD and ERS subpart of trial
+%       32 samples (32 pwelch power) on 19freq and 16channel = 304 features
+%       17 first samples are ERD (CLASS 0) and 17 last ERS (CLASS 1)
+
+%Loop over trial
+nTrials = size(trials,1);
+
+features_mat = []
+
+for itrial = 1:nTrials
+    thisTrial = trials(itrial,:,:);
+    feat_oneTrial_ERD = windowing(thisTrial, time, win, shift, start_ERD, stop_ERD);
+    %17 x 304
+    feat_oneTrial_ERS = windowing(thisTrial, time, win, shift, start_ERS, stop_ERS);
+
+    feat_oneTrial = cat(1,feat_oneTrial_ERD,feat_oneTrial_ERS); %32 x 304
+    
+    features_mat = cat(3,features_mat,feat_oneTrial);
+    % 32 x 304 x 80   =  samples x Features x nTrials
+
+end
+
+end
+
---
+++ Code/5_feature_extraction/windowing.m
@@ -0,0 +1,51 @@
+function [feat_oneTrial] = windowing(trial, time, win, shift, start, stop)
+%Perform sequential Pwelch over 1s on a given subpart of a given trial, for
+%Feature Extraction
+%  
+%   Input :
+%       - trial : a trial output from epoching. 
+%                   Dim (trial x Channel x time) (1 x 16 x [time window of
+%                   2 s])
+%                   WARNING : Need an epoching -3 +3 aroung MI_STOP event
+%       - time : time value associated with the epoch
+%       - win : time window in s on wich pwelch is compute
+%       - shift : time overlap in s to shift windwo and compute next Pwelch
+%       - start : Time in second from EVENT, where to start feature extr.
+%       - stop : Time in second from EVENT, where to stop feature extr.
+%                If MI-STOP event:
+%                   ERD (Class 0) : start = -2   stop = 0
+%                   ERS (Class 1) : start = 0.5 stop = 2.5
+%
+%
+%   Output :
+%       feat_oneTrial : 17 x 304     for each 17 sequential Pwelch (each Pwelch 1s, shifted 0.0625, , 304
+%       features values containing 19 power values (for freq 4:2:40) for
+%       all 16 channels
+%
+
+% Extract the subpart of interest (ERD or ERS) from the whole epoch
+%WARNING : Need an epoching -3 +3 aroung MI_STOP event
+    sampling_rate = 512;
+    time = time + (ones(length(time))*-3);
+    start_ind = find(time==start);
+    data = squeeze(trial); % 16 x time 
+   
+    power = [];
+    %Shift sequentially window
+    shift_nsample = shift*sampling_rate; %32
+    n_win = (stop-start)/shift; %16
+    for iShift = 0:n_win
+        start_ind = start_ind + iShift*shift_nsample;
+        stop_ind = start_ind + win*sampling_rate;
+            
+        %pwelch compute PSD on each column
+        [pxx, f] = pwelch(transpose(data(:,start_ind:stop_ind)), sampling_rate, 0.5*sampling_rate, [4:2:40], sampling_rate);
+        %pxx = 19x16   PSD for (freq x eachChannel)
+        pxx = reshape(pxx, [1,[]]); %1 x 304 
+    
+    end
+    
+    feat_oneTrial = cat(1,power,pxx);  % 17 x 304    (window_shifted x features) = (samples * features)
+
+end
+
